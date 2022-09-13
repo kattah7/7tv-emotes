7df65db9015f12aabd4b896ec8ca25aeb18aa765
Kattah <kattahpro@gmail.com>
2022-09-14 01:27:45 +0900

    added global API

---
+++ src/api/routes/global.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import fetch from 'node-fetch';
+const router = express.Router();
+
+router.get('/global', async (req, res) => {
+    const globalEmotes = await fetch('http://localhost:5002/global').then((res) => res.json());
+    return res.status(200).json({
+        success: true,
+        data: globalEmotes,
+    });
+});
+
+export default router;
