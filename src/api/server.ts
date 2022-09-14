3851767e02b189f0b9d43e5e37f7229a36bab8d1
Kattah <kattahpro@gmail.com>
2022-09-14 17:06:47 +0900

    removed global

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import channelInfo from './routes/channelInfo';
-import global from './routes/global';
 import * as Logger from '../utility/logger';
 import { bot } from '../../config.json';
 
@@ -10,7 +9,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(channelInfo);
-app.use(global);
 app.listen(bot.port, () => {
     Logger.info('Server is running on port ' + bot.port);
 });
