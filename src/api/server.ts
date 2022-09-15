99334e81cd77c5bc76d0230d8a2d7c4532f2fe79
Kattah <kattahpro@gmail.com>
2022-09-15 20:50:49 +0900

    added back global

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import channelInfo from './routes/channelInfo';
+import global from './routes/global';
 import * as Logger from '../utility/logger';
 import { bot } from '../../config.json';
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(channelInfo);
+app.use(global);
 app.listen(bot.port, () => {
     Logger.info('Server is running on port ' + bot.port);
 });
