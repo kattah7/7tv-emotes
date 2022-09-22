f2d8c1b47b8a39127a412de0fd351d9190f101e2
Kattah <kattahpro@gmail.com>
2022-09-23 03:49:57 +0900

    api filter by true

--- src/api/routes/channelInfo.ts
+++ src/api/routes/channelInfo.ts
@@ -29,7 +29,8 @@ router.get('/c/:user', async (req, res) => {
         };
     });
 
-    const mapByTopUsage = emotesMapped.sort((a, b) => b.usage - a.usage);
+    const filterEmotesByTrue = emotesMapped.filter((emote: any) => emote.isEmote === true);
+    const mapByTopUsage = filterEmotesByTrue.sort((a, b) => b.usage - a.usage);
     const sliceHundred = mapByTopUsage.slice(0, 100);
 
     return res.status(200).json({
