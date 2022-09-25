b3049077087ce0c5ea7068e1569a4831f0d4a18d
Kattah <kattahpro@gmail.com>
2022-09-25 14:58:22 +0900

    any

--- src/api/routes/topemotes.ts
+++ src/api/routes/topemotes.ts
@@ -5,23 +5,23 @@ const router = express.Router();
 router.get('/top', async (req, res) => {
     const allEmotes = await Emote.find();
     const PushAllEmotes = [];
-    allEmotes.forEach((emote) => {
-        emote.emotes.forEach((emote) => {
+    allEmotes.forEach((emote: any) => {
+        emote.emotes.forEach((emote: any) => {
             PushAllEmotes.push(emote);
         });
     });
-    const sortedEmotes = PushAllEmotes.sort((a, b) => b.usage - a.usage);
+    const sortedEmotes = PushAllEmotes.sort((a: any, b: any) => b.usage - a.usage);
     const topEmotes = sortedEmotes.slice(0, 25);
     const ifEmoteIdIsSameCombine = [];
-    topEmotes.forEach((emote) => {
-        const doesEmoteExist = ifEmoteIdIsSameCombine.find((emote2) => emote2.emote == emote.emote);
+    topEmotes.forEach((emote: any) => {
+        const doesEmoteExist = ifEmoteIdIsSameCombine.find((emote2: any) => emote2.emote == emote.emote);
         if (!doesEmoteExist) {
             ifEmoteIdIsSameCombine.push(emote);
         } else {
             doesEmoteExist.usage += emote.usage;
         }
     });
-    const emotesMapped = ifEmoteIdIsSameCombine.map((emote) => {
+    const emotesMapped = ifEmoteIdIsSameCombine.map((emote: any) => {
         return {
             name: emote.name,
             emote: emote.emote,
