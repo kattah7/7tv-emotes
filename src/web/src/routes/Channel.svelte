a689216508c962d15b87114949aa1a13888291b2
Kattah <kattahpro@gmail.com>
2022-10-25 11:32:28 +0900

    added parse int

--- src/web/src/routes/Channel.svelte
+++ src/web/src/routes/Channel.svelte
@@ -42,9 +42,7 @@
         if (count !== null) {
             channelEmotes.forEach((emote) => {
                 if (emote.name === emoteName) {
-                    const findThatEmote = document.getElementById(emoteName);
-                    const realUsage = emote.usage + count;
-                    findThatEmote.innerHTML = realUsage + count;
+                    const realUsage = parseInt(emote.usage + count);
                     for (let i = 0; i < channelEmotes.length; i++) {
                         if (channelEmotes[i].name === emoteName) {
                             channelEmotes[i].usage = realUsage;
