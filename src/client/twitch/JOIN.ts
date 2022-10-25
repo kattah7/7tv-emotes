6f9a795b50a47955d10a0b6a39a3f93788abed67
Kattah <kattahpro@gmail.com>
2022-10-25 22:58:55 +0900

    moved sending 7tv ws

--- src/client/twitch/JOIN.ts
+++ src/client/twitch/JOIN.ts
@@ -1,41 +1,40 @@
 import { client } from '../../utility/connections';
 import { Emote } from '../../utility/db';
 import { StvInfo, UserInfo } from '../../utility/parseUID';
-import { channelEmotes } from '../../utility/channelEmotes';
+import { WS } from '../../utility/stv';
 import fs from 'fs/promises';
 import * as Logger from '../../utility/logger';
 
 async function JOIN() {
     client.on('JOIN', async ({ channelName }) => {
-        Logger.info(`Joined ${channelName}`);
-        const userID = (await UserInfo(channelName))[0].id;
-        if (!(await Emote.findOne({ id: userID }))) {
-            const channelEmote = await channelEmotes(userID);
-            if (channelEmote == null) {
-                Logger.error(`No emotes found for ${channelName}`);
-                return;
-            }
-            try {
-                const newEmote = new Emote({
-                    name: channelName.toLowerCase(),
-                    id: userID,
-                    StvId: (await StvInfo(userID)).user.id,
-                    emotes: channelEmote,
-                });
-                await newEmote.save();
-                Logger.warn(`User doesn't exist in DB, adding... ${channelName} emotes`);
-            } catch (err) {
-                Logger.error(err);
-            }
+        function sendWS(op: number, type: string, id: string) {
+            WS.send(
+                JSON.stringify({
+                    op: op,
+                    d: {
+                        type: type,
+                        condition: {
+                            object_id: id,
+                        },
+                    },
+                })
+            );
         }
 
+        Logger.info(`Joined ${channelName}`);
+        const { id } = (await UserInfo(channelName))[0];
+        const { user, emote_set } = await StvInfo(id);
+        if (!user && !emote_set) return;
+        sendWS(35, 'user.update', user.id);
+        sendWS(35, 'emote_set.update', emote_set.id);
+
         const knownEmoteNames = new Set(
-            (await Emote.findOne({ id: userID })).emotes
+            (await Emote.findOne({ id: id })).emotes
                 .filter((emote) => emote.isEmote === true)
                 .map((emote) => emote.name)
         );
-        fs.writeFile(`./src/stats/${userID}.json`, JSON.stringify([...knownEmoteNames]));
-        Logger.info(`Wrote ${knownEmoteNames.size} emotes to ${userID}.json`);
+        fs.writeFile(`./src/stats/${id}.json`, JSON.stringify([...knownEmoteNames]));
+        Logger.info(`Wrote ${knownEmoteNames.size} emotes to ${id}.json`);
     });
 }
 
--- src/client/twitch/PART.ts
+++ src/client/twitch/PART.ts
@@ -1,9 +1,30 @@
 import { client } from '../../utility/connections';
+import { StvInfo, UserInfo } from '../../utility/parseUID';
+import { WS } from '../../utility/stv';
 import * as Logger from '../../utility/logger';
 
 async function PART() {
-    client.on('PART', ({ channelName }) => {
+    client.on('PART', async ({ channelName }) => {
+        function sendWS(op: number, type: string, id: string) {
+            WS.send(
+                JSON.stringify({
+                    op: op,
+                    d: {
+                        type: type,
+                        condition: {
+                            object_id: id,
+                        },
+                    },
+                })
+            );
+        }
+
         Logger.info(`Parted ${channelName}`);
+        const { id } = (await UserInfo(channelName))[0];
+        const { user, emote_set } = await StvInfo(id);
+        if (!user && !emote_set) return;
+        sendWS(36, 'user.update', user.id);
+        sendWS(36, 'emote_set.update', emote_set.id);
     });
 }
 
