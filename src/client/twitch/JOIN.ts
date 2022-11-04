bbd1a2527d15fc8a83b625e7525b26f8efeb21b1
Kattah <kattahpro@gmail.com>
2022-11-04 21:48:24 +0900

    Less RAM usage

--- src/client/twitch/JOIN.ts
+++ src/client/twitch/JOIN.ts
@@ -10,12 +10,6 @@ async function JOIN() {
         Logger.info(`Joined ${channelName}`);
         const { id } = (await UserInfo(channelName))[0];
 
-        const knownEmoteNames = new Set(
-            (await Emote.findOne({ id: id })).emotes
-                .filter((emote) => emote.isEmote === true)
-                .map((emote) => emote.name)
-        );
-
         const emoteUsage = (await Emote.findOne({ id: id })).emotes.filter((emote) => isNaN(emote.usage));
         if (emoteUsage[0]) {
             for (const emote of emoteUsage) {
@@ -26,39 +20,96 @@ async function JOIN() {
                 Logger.info(`Reset usage for ${emote.emote} in ${channelName}`);
             }
         }
-        if (knownEmoteNames.size === 0) return;
-        await fs.writeFile(`./src/stats/${id}.json`, JSON.stringify([...knownEmoteNames]));
+
+        async function emoteInfo(boolean: boolean, type: any, doFilter: boolean) {
+            const emotes = new Set(
+                (await Emote.findOne({ id: id })).emotes
+                    .filter((emote) => (doFilter ? emote.isEmote === boolean : emote))
+                    .map((emote) => emote[type])
+            );
+            return emotes;
+        }
+
+        await fs.writeFile(`./src/stats/${id}.json`, JSON.stringify([...(await emoteInfo(true, 'name', true))]));
+
+        const filteredEmotes = await emoteInfo(true, 'emote', false);
+        const deletedEmotesQuery = await emoteInfo(false, 'emote', true);
 
         const { emote_set } = await getEmotes(id);
         if (!emote_set.emotes) return;
+
+        let emotes = [];
+        let deletedEmotes = [];
         for (const emote of emote_set.emotes) {
-            const emoteDB = await Emote.findOne({ id: id }).exec();
-            const emoteIDS = emoteDB?.emotes.map((emote) => emote.emote);
-            Logger.info(`querying ${emote.name} in ${channelName}`);
-            if (emoteDB && emoteIDS?.includes(emote.id)) continue;
-            await Emote.findOneAndUpdate(
-                { id: id },
-                {
-                    $push: {
-                        emotes: {
-                            name: emote.name,
-                            emote: emote.id,
-                            usage: 1,
-                            isEmote: true,
-                            Date: Date.now(),
-                        },
-                    },
-                },
-                { upsert: true, new: true }
-            ).exec();
-            const existtingEmoteNames = new Set(
-                (await Emote.findOne({ id: id })).emotes
-                    .filter((emote) => emote.isEmote === true)
-                    .map((emote) => emote.name)
-            );
-            await fs.writeFile(`./src/stats/${id}.json`, JSON.stringify([...existtingEmoteNames]));
-            Logger.info(`Added ${emote.name} to ${channelName}'s emotes`);
+            if (!filteredEmotes.has(emote.id)) {
+                emotes.push(emote);
+            } else {
+                deletedEmotes.push(emote);
+                if (deletedEmotesQuery.has(emote.id)) {
+                    await Emote.updateOne(
+                        { 'id': id, 'emotes.emote': emote.id },
+                        { $set: { 'emotes.$.isEmote': true } }
+                    ).exec();
+                    Logger.info(`Set ${emote.name} back to true in ${channelName}`);
+                }
+            }
         }
+
+        const filterEmotes = [...filteredEmotes].filter(
+            (emote) => !deletedEmotes.map((emote) => emote.id).includes(emote)
+        );
+        const emotesByIDAndTrue = await emoteInfo(true, 'emote', true);
+
+        if (filterEmotes.length > 0) {
+            for (const emote of filterEmotes) {
+                if (emotesByIDAndTrue.has(emote)) {
+                    await Emote.updateOne(
+                        { 'id': id, 'emotes.emote': emote },
+                        { $set: { 'emotes.$.isEmote': false } }
+                    ).exec();
+                    Logger.info(`Set ${emote} to false in ${channelName}`);
+                }
+            }
+        }
+
+        for (const { name, id: emoteID } of emotes) {
+            await Emote.updateOne(
+                { id: id },
+                { $push: { emotes: { name: name, emote: emoteID, usage: 1, isEmote: true, Date: Date.now() } } }
+            ).exec();
+            Logger.info(`Added ${name} to ${channelName}`);
+        }
+
+        const emoteNames = await emoteInfo(true, 'name', true);
+        await fs.writeFile(`./src/stats/${id}.json`, JSON.stringify([...emoteNames]));
+        // for (const emote of emote_set.emotes) {
+        //     const emoteDB = await Emote.findOne({ id: id }).exec();
+        //     const emoteIDS = emoteDB?.emotes.map((emote) => emote.emote);
+        //     Logger.info(`querying ${emote.name} in ${channelName}`);
+        //     if (emoteDB && emoteIDS?.includes(emote.id)) continue;
+        //     await Emote.findOneAndUpdate(
+        //         { id: id },
+        //         {
+        //             $push: {
+        //                 emotes: {
+        //                     name: emote.name,
+        //                     emote: emote.id,
+        //                     usage: 1,
+        //                     isEmote: true,
+        //                     Date: Date.now(),
+        //                 },
+        //             },
+        //         },
+        //         { upsert: true, new: true }
+        //     ).exec();
+        //     const existtingEmoteNames = new Set(
+        //         (await Emote.findOne({ id: id })).emotes
+        //             .filter((emote) => emote.isEmote === true)
+        //             .map((emote) => emote.name)
+        //     );
+        //     await fs.writeFile(`./src/stats/${id}.json`, JSON.stringify([...existtingEmoteNames]));
+        //     Logger.info(`Added ${emote.name} to ${channelName}'s emotes`);
+        // }
     });
 }
 
