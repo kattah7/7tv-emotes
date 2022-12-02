27ed8b2cd9be4f8aa5143b54fe3df352a8741ff8
Kattah <kattahpro@gmail.com>
2022-12-03 02:48:39 +0900

    vi von

--- src/utility/parseUID.ts
+++ src/utility/parseUID.ts
@@ -2,13 +2,23 @@ import fetch from 'node-fetch';
 
 export async function UserInfo(username: string) {
     if (!username) return null;
-    const Data = await fetch(`https://api.ivr.fi/v2/twitch/user?login=${username}`).then((res) => res.json());
+    const Data = await fetch(`https://api.ivr.fi/v2/twitch/user?login=${username}`, {
+        method: 'GET',
+        headers: {
+            'User-Agent': 'IF YOU SEE THIS VI VON ZULUL',
+        },
+    }).then((res) => res.json());
     if (Data.length === 0) return null;
     return Data;
 }
 
 export async function StvInfo(username: string) {
     if (!username) return null;
-    const Data = await fetch(`https://7tv.io/v3/users/twitch/${username}`).then((res) => res.json());
+    const Data = await fetch(`https://7tv.io/v3/users/twitch/${username}`, {
+        method: 'GET',
+        headers: {
+            'User-Agent': 'IF YOU SEE THIS VI VON ZULUL',
+        },
+    }).then((res) => res.json());
     return Data;
 }
