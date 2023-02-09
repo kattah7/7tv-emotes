3fa376cdef31b831ad3b8958b3d91ba01abf5e9f
Kattah <kattahpro@gmail.com>
2023-02-10 02:31:31 +0900

    Added back emote transfer

--- Backend/server/Transfer.ts
+++ Backend/server/Transfer.ts
@@ -19,20 +19,20 @@ export async function Transfer(): Promise<void> {
 		await Bot.SQL.Query(`INSERT INTO channels (twitch_username, twitch_id, stv_id) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING`, [xd.name, xd.id, xd.StvId]);
 		Bot.Logger.Warn(`[${current++}/${count}] ${xd.name} (${xd.id})`);
 
-		// for (const emote of xd.emotes) {
-		// 	if (emote.name.includes('Emote Removed') || !emote.emote || typeof emote.usage !== 'number' || isNaN(emote.usage)) continue;
+		for (const emote of xd.emotes) {
+			if (emote.name.includes('Emote Removed') || !emote.emote || typeof emote.usage !== 'number' || isNaN(emote.usage)) continue;
 
-		// 	const Timestamp = emote.Date;
-		// 	emote.Date = new Date(Timestamp);
+			const Timestamp = emote.Date;
+			emote.Date = new Date(Timestamp);
 
-		// 	const formatedDate = emote.Date.toISOString();
-		// 	await Bot.SQL.Query(
-		// 		`INSERT INTO emotes (twitch_id, emote, emote_id, emote_count, added)
-		//   		 VALUES ($1, $2, $3, $4, $5)
-		//   		 ON CONFLICT (twitch_id, emote_id)
-		//   		 DO UPDATE SET emote = $2, emote_count = $4 WHERE emotes.emote != $2`,
-		// 		[xd.id, emote.name, emote.emote, emote.usage, formatedDate],
-		// 	);
-		// }
+			const formatedDate = emote.Date.toISOString();
+			await Bot.SQL.Query(
+				`INSERT INTO emotes (twitch_id, emote, emote_id, emote_count, added)
+		  		 VALUES ($1, $2, $3, $4, $5)
+		  		 ON CONFLICT (twitch_id, emote_id)
+		  		 DO UPDATE SET emote = $2, emote_count = $4 WHERE emotes.emote != $2`,
+				[xd.id, emote.name, emote.emote, emote.usage, formatedDate],
+			);
+		}
 	}
 }
