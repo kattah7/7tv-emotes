b076800f3b40d35384288e7139668c688806707d
Zonian <57513632+ZonianMidian@users.noreply.github.com>
2023-10-08 14:17:58 -0500

    Frontend removed

--- Frontend/src/utils/Fetch.ts
+++
@@ -1,5 +0,0 @@
-export async function Fetch(url: string, options: any = {}): Promise<any> {
-  const res = await fetch(url, options);
-  const json = await res.json();
-  return json;
-}
--- Frontend/src/utils/HandleInput.ts
+++
@@ -1,7 +0,0 @@
-export function HandleInput(event: Event): void {
-  let { value } = event.target[0];
-  if (value.length === 0) return;
-
-  value = value.toLowerCase().replace(/ /g, "");
-  window.location.href = `/c/${value}`;
-}
